68688ba43e440a5e24c16a534859485ad413bedd
SgtGarro <marcelogarro137@gmail.com>
2022-12-02 22:09:56 -0500

    Return functions

---
+++ src/13-return-functions.ts
@@ -0,0 +1,12 @@
+(() => {
+  const calcTotal = (prices: number[]): number => {
+    return prices.reduce((sum, tot) => (tot += sum), 0);
+  };
+
+  const printTotal = (prices: number[]): void => {
+    const rta = calcTotal(prices);
+    console.log(`Total is ${rta}`);
+  };
+
+  printTotal([1, 2, 1, 1, 1]);
+})();
